ca4ab407b4fac103e46ff9fbd9f45fe0da315d9b
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-09-12 23:48:16 +0200

    prepping lfpykernels-0.1.1 (#63)
    * prepping lfpykernels-0.1.1
    
    * linting

--- lfpykernels/version.py
+++ lfpykernels/version.py
@@ -1,1 +1,11 @@
-version = "0.1"
+_MAJOR = "0"
+_MINOR = "1"
+# On main and in a nightly release the patch should be one ahead of the last
+# released build.
+_PATCH = "1"
+# This is mainly for nightly builds which have the suffix ".dev$DATE". See
+# https://semver.org/#is-v123-a-semantic-version for the semantics.
+_SUFFIX = ""
+
+version_short = "{0}.{1}".format(_MAJOR, _MINOR)
+version = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
