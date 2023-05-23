337f37b6929e4c85f3e4790bc1442e2866f8d3d8
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-05-23 09:36:36 +0200

    Release 0.2 Fixes #72

--- lfpykernels/version.py
+++ lfpykernels/version.py
@@ -2,10 +2,10 @@ _MAJOR = "0"
 _MINOR = "2"
 # On main and in a nightly release the patch should be one ahead of the last
 # released build.
-_PATCH = "0"
+_PATCH = ""
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
-_SUFFIX = "dev"
+_SUFFIX = ""
 
 version_short = "{0}.{1}".format(_MAJOR, _MINOR)
 version = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
